cd3c3d25a3872735a2e143f800c34f618ec9db31
nemisgerry <gerrymaths@gmail.com>
2017-05-16 23:20:45 +0000

    Update

--- Tarea5/construir_vocabulario.py
+++ Tarea5/construir_vocabulario.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 from code_hw import ClassifyImages
 
 #Primero se va a construir el vocabulario.
 #El archivo se llama "imagedb.zip".
-#El número de iteraciones es 100.
+#El numero de iteraciones es 100.
 #El nombre del modelo que se va a obtener es "vocabulario_sin_pca.pkl".
 #La bandera es False ya que no se aplicara PCA a los vectores sift.
 #El ultimo valor es el nombre de el PCA que se debio de haber obtenido anteriormente.
