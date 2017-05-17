3bb0e86f2274fd5d8bf785b44657b5afcd742ef2
nemisgerry <gerrymaths@gmail.com>
2017-05-16 19:28:20 -0500

    Add the file that creates the bag of features

---
+++ Tarea5/generate_bag_features.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from code_hw import ClassifyImages
+
+#Primero se va a construir el vocabulario.
+#El archivo se llama "imagedb.zip".
+#El numero de iteraciones es 100.
+#El nombre del modelo que se va a obtener es "vocabulario_sin_pca.pkl".
+#La bandera es False ya que no se aplicara PCA a los vectores sift.
+#El ultimo valor es el nombre de el PCA que se debio de haber obtenido anteriormente.
+ClassifyImages.generate_bag_of_features("imagedb.zip","vocabulario.pkl","bolsas_caracteristicas_rpeuba.pkl")
